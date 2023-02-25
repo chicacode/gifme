7cd29d45b4a24e560dbca9d081a2b5059cec01e4
Geraldine <gerahernan.al@gmail.com>
2023-02-24 19:50:06 -0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GIFME APP
+
+## Recreate Giphy with their API (https://developers.giphy.com/docs/api)
+
+- The goal of this project is to build a beautiful page that
+you can use to find GIFs. 
+
+- Use Giphy API.
+
+- Select favorites Giphy.
---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* 
+Recreate Giphy with their API (https://developers.giphy.com/docs/api)
+
+API Key=2I8f4Stngla6Yy7O5DcMgzbaUirHKQOc
+https://api.giphy.com/v1/gifs/search?api_key=2I8f4Stngla6Yy7O5DcMgzbaUirHKQOc&q=pug&limit=25&offset=0&rating=g&lang=en
+
+https://api.giphy.com/v1/gifs/trending?api_key=2I8f4Stngla6Yy7O5DcMgzbaUirHKQOc&limit=25&rating=pg-13
+
+https://api.giphy.com/v1/gifs/trending?api_key=2I8f4Stngla6Yy7O5DcMgzbaUirHKQOc&limit=25&rating=r
+
+*/
