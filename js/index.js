c61e4c8952fe148ffd25d39e49e42cfcd3f48925
Geraldine <gerahernan.al@gmail.com>
2023-02-26 13:24:47 -0800

    feat: added logic show ui and call api search giphy

--- js/index.js
+++ js/index.js
@@ -12,7 +12,10 @@ https://api.giphy.com/v1/gifs/trending?api_key=2I8f4Stngla6Yy7O5DcMgzbaUirHKQOc&
 
 console.log("Hello Giphy")
 const results = document.querySelector(".box-container");
+
+const searchForm = document.querySelector("#search");
 const searchKeyword = document.getElementById("search-input");
+const noResultBanner = document.getElementById("no-result");
 
 const config = {
     baseUrl: "https://api.giphy.com/v1/gifs",
@@ -20,26 +23,116 @@ const config = {
 }
 
 let trendingGiphs = [];
+let allGiphsList = []
 
 // URLS
-const TRENDING_GIPHS_URL = `${config.baseUrl}/trending?${config.apiKey}&limit=25&rating=pg-13`;
-const SEARCH_GIPHS_URL = `${config.baseUrl}/search?${config.apiKey}`;
+const TRENDING_GIPHY_URL = `${config.baseUrl}/trending?${config.apiKey}`;
+const SEARCH_GIPHY_URL = `${config.baseUrl}/search?${config.apiKey}`;
+const RANDOM_GIPHY_URL = `${config.baseUrl}/random?${config.apiKey}`;
 
-const getGiphData = async (url, keyword) =>{
 
-    const apiURL = keyword ? `${url}&q=${keyword}&limit=25&offset=0&rating=g&lang=en` : url;
+const showUI = (data) => {
+
+    while(results.firstChild){
+        results.removeChild(results.firstChild)
+    }
+
+    data.forEach(({ type, id, url, title, rating, images }) => {
+        let divContainer = document.createElement('div');
+        let img = document.createElement('img');
+        let name = document.createElement('p');
+        let typeContainer = document.createElement('span');
+        let ratingContainer = document.createElement('div');
+        let textContainer = document.createElement('div');
+
+        img.src = images.original.url;
+        name.textContent = `${title}`;
+        typeContainer.textContent = `${type}`;
+        ratingContainer.textContent = `${rating}`;
+        divContainer.classList.add("card-box-container");
+        divContainer.appendChild(img);
+        textContainer.appendChild(name);
+
+        divContainer.appendChild(textContainer);
+
+        results.appendChild(divContainer);
+    
+    })
+}
+
+const parseList = (array) => {
+    return array.map(({ type, id, url, title, rating, images }) => {
+        return {
+            type: type,
+            id: id,
+            url: url,
+            title: title,
+            rating: rating,
+            images: images
+        }
+    })
+
+}
+
+const getGiphData = async (url, keyword) => {
+
+    const apiURL = keyword ? `${url}&q=${keyword}&limit=30&offset=0&rating=g&lang=en` : url;
     console.log("apiURL", apiURL)
-    try{
+    try {
         const data = await fetch(apiURL);
         const response = await data.json();
-    
-        console.log(response)
-    }catch(error){
+        allGiphsList = parseList(response.data);
+        showUI(allGiphsList)
+    } catch (error) {
         console.error("error", error)
     }
 }
 
-document.addEventListener("DOMContentLoaded", () =>{
-    getGiphData(TRENDING_GIPHS_URL)
+document.addEventListener("DOMContentLoaded", () => {
+    getGiphData(TRENDING_GIPHY_URL)
+});
+
+const validateForm = (e) => {
+    e.preventDefault();
+
+    const searchKeywordValue = searchKeyword.value;
+
+    if (searchKeywordValue === '') {
+        showNoResultBanner('add valid value');
+        return false;
+    }
+
+}
+
+const showNoResultBanner = (message) => {
+    const noResult = document.createElement("p");
+    const noAlert = document.querySelector("alert");
+
+    if (!noAlert) {
+        noResult.classList.add("alert", "error");
+        noResult.innerHTML = `  <strong>Error</strong>
+        <span class="block">${message}</span>`;
+        noResultBanner.appendChild(noResult);
+
+        setTimeout(() => {
+            noResult.remove();
+        }, 3000)
+    }
+
+}
+
+// Events:
+
+searchKeyword.addEventListener("keyup", e => {
+    console.log("entering value", e.target.value)
+    searchGiphy()
 })
 
+const searchGiphy = async () => {
+    // const result = movieList.filter(filterByName).filter(filterByYear)
+    const searchKeywordValue = searchKeyword.value;
+
+    let { data } = await getGiphData(SEARCH_GIPHY_URL, searchKeywordValue);
+    console.log("searched data", data)
+    // return generateUI(result);
+  }
